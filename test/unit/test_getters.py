eb4974fc74a02643a0ccd52ad4686ab71f06b729
Anas Badaha <anasb@mellanox.com>
2019-03-03 07:48:58 +0000

    Add Unit Test for get_facts
    Signed-off-by: Anas Badaha <anasb@mellanox.com>

--- test/unit/test_getters.py
+++ test/unit/test_getters.py
@@ -1,7 +1,9 @@
 """Tests for getters."""
 
 from napalm.base.test.getters import BaseTestGetters
-
+from napalm.base.test import helpers
+from napalm.base.test.getters import wrap_test_cases
+from napalm.base.utils.py23_compat import text_type
 
 import pytest
 
@@ -9,3 +11,18 @@ import pytest
 @pytest.mark.usefixtures("set_device_parameters")
 class TestGetter(BaseTestGetters):
     """Test get_* methods."""
+
+    @wrap_test_cases
+    def test_get_facts(self, test_case):
+        """Test get_facts method."""
+        modale_facts = {
+            "os_version": text_type,
+            "uptime": int,
+            "interface_list": list,
+            "vendor": text_type,
+            "model": text_type,
+            "hostname": text_type,
+        }
+        facts = self.device.get_facts()
+        assert helpers.test_model(modale_facts, facts)
+        return facts
