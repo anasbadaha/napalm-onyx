a714184bde8a50abe10cc72ae8d12baf6110e056
Anas Badaha <anasb@mellanox.com>
2019-03-03 09:40:10 +0000

    Adding Support for Python 3.4, 3.5
    Signed-off-by: Anas Badaha <anasb@mellanox.com>

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setup(
          'Programming Language :: Python',
          'Programming Language :: Python :: 2',
          'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3.4',
+         'Programming Language :: Python :: 3.5',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
     ],
